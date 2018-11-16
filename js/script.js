a8a8b8e16bcb138b534e130aa6102a75668098f1
Shomail <shomail1988@gmail.com>
2018-11-16 16:26:05 +0100

    responsive fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.querySelector('.user-nav__user').addEventListener('click', (e) => {
+  e.stopPropagation();
   const dropdown = document.querySelector('.user-nav__user > .dropdown');
 
   if(dropdown.classList.contains('dropdown--active')) {
@@ -6,4 +7,8 @@ document.querySelector('.user-nav__user').addEventListener('click', (e) => {
   }else {
     dropdown.classList.add('dropdown--active')
   }
+})
+
+document.body.addEventListener('click', (e) => {
+  document.querySelector('.dropdown').classList.remove('dropdown--active');
 })
